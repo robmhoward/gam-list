73b2311d3f7cc286392694bb644904c5cfe012cd
Rob Howard <robmhoward@hotmail.com>
2016-05-14 10:00:54 -0700

    Initial commit

---
+++ script/app.js
@@ -0,0 +1,31 @@
+/*Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.*/
+var gameListApp = angular.module("gamListApp", ['ngRoute']);
+
+gameListApp.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/apps',
+			{
+				controller: 'AppsController',
+				templateUrl: 'partials/apps-browser.html'
+			})
+		.otherwise({redirectTo: '/apps' });
+}]);
+
+gameListApp.factory("appsFactory", ['$http', function ($http) {
+	var factory = {};
+	
+	factory.getApps = function() {
+		return $http.get('data.json');
+	};
+
+	return factory;
+}]);
+
+gameListApp.controller("AppsController", function($scope, $routeParams, appsFactory) {
+	$scope.samples = [{ name: "Loading..." }];
+		
+	appsFactory.getApps().then(function (response) {
+		$scope.apps = response.data.apps;
+	});
+    	
+});
